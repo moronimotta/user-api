cb8b4e3d121cb280ed4e1092d0f545ce2000668d
moronimotta <dareevil7@gmail.com>
2025-05-08 16:09:47 -0300

    Role and AuthKey added to user entity

--- user/entities/entity.go
+++ user/entities/entity.go
@@ -17,12 +17,15 @@ type User struct {
 	CreatedAt string `json:"created_at"`
 	UpdatedAt string `json:"updated_at"`
 	DeletedAt string `json:"deleted_at"`
-	Role      string `json:"role"`
+	Role      string `json:"role" gorm:"default:'user'"` // default role is user
+	AuthKey   string `json:"auth_key"`
 	Avatar    string `json:"avatar"` //It will be saved in the S3 bucket
+
 }
 
 func (u *User) BeforeCreate() error {
 	u.ID = uuid.New().String()
+	u.AuthKey = uuid.New().String()
 	u.CreatedAt = time.Now().Format("2006-01-02 15:04:05")
 	u.UpdatedAt = time.Now().Format("2006-01-02 15:04:05")
 
